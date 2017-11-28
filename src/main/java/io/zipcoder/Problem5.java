09cbe4d64cb2e9df315a304f9a6ea23e08733ac4
jeffrey <jeffrey@zipcoders-MacBook-Pro-3.local>
2017-11-28 10:40:28 -0500

    Not even close

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -1,4 +1,19 @@
 package io.zipcoder;
 
 public class Problem5 {
+
+    public static String reverseString5(String input){
+
+       // String[] inpout = new String[input]; Started this at the end.
+
+        for(int i = input.length(); i > 0; i--){
+            input = input + Integer.toString(i);
+           // System.out.println(input);
+
+        }
+        return input;
+
+
+
+    }
 }
--- src/test/java/io/zipcoder/Problem5Test.java
+++ src/test/java/io/zipcoder/Problem5Test.java
@@ -1,4 +1,21 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem5Test {
+
+    @Test
+    public void reverseString5Test() {
+        //String input ="Jeff";
+        String expected = "ffej";
+        String actual = Problem5.reverseString5("jeff");
+        Assert.assertEquals(expected,actual);
+
+
+
+
+
+    }
+
 }
